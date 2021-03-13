43e9b7387a3f2d51a143aa0a45a74a35827f26d3
Adrian DeRose <adrian.derose6@gmail.com>
2021-03-13 11:51:06 -0600

    Add header styles

--- src/website.js
+++ src/website.js
@@ -13,6 +13,18 @@ function loadNav() {
     });
 }
 
+function runOnScroll() {
+    const header = document.getElementById('header');
+    let top = window.scrollY;
+    console.log(top);
+    if (top >= 100) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+    console.log('scrolling')
+ }; 
+
 function setActiveButton(button) {
     const buttons = document.querySelectorAll('.button-nav');
 
@@ -24,13 +36,6 @@ function setActiveButton(button) {
 
     button.classList.add('active-button');
     console.log('Set Active')
-}
-
-function createMain() {
-    const main = document.createElement('main');
-    main.classList.add('main');
-    main.setAttribute('id', 'main');
-    return main;
 }
 
 function createFooter() {
@@ -58,9 +63,10 @@ function initializeWebsite() {
     const content = document.getElementById('content');
 
     loadNav();
-    content.appendChild(createMain());
     content.appendChild(createFooter());
     loadHome();
+
+    window.addEventListener("scroll", runOnScroll);
 }
 
 export default initializeWebsite;
